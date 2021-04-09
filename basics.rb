85aaed7bc453a414de438ff714e5ed6a84015527
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-09 17:52:45 +1000

    Create basics.rb

---
+++ basics.rb
@@ -0,0 +1,9 @@
+# Variable and methods
+var = "Hello there"
+var.reverse
+puts var
+
+# puts vs print
+puts "Creates a new line after printing, similar to Pythons print"
+print "Will allow other console logs to"
+print " appear right next to it
