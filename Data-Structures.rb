6365a4284e4a860895aba84343445146a4cc51e9
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-11 14:07:46 +1000

    Update Data-Structures.rb

--- Data-Structures.rb
+++ Data-Structures.rb
@@ -1,2 +1,17 @@
 # Multidimensional Array
 my_array = [[1,2],[3,4]]
+
+#Hash
+my_hash = Hash.new
+my_hash["Copper"] = "doggo"
+
+#Iterating a Hash
+lunch_order = {
+  "Ryan" => "wonton soup",
+  "Eric" => "hamburger",
+  "Jimmy" => "sandwich",
+  "Sasha" => "salad",
+  "Cole" => "taco"
+}
+
+lunch_order.each do |i, j| puts "#{j}" end # Only prints the value, so not i
