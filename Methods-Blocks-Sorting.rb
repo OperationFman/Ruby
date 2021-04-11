f8fd87aa001138ff1962b985fbee2fa49881b14b
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-11 15:23:08 +1000

    Create Methods-Blocks-Sorting.rb

---
+++ Methods-Blocks-Sorting.rb
@@ -0,0 +1,24 @@
+#Methods
+def greeter(name)
+    return "Welcome #{name}"
+  end
+  
+  def by_three?(number)
+    if number % 3 == 0
+      return true
+  
+      return false
+    end
+  end
+
+# Blocks
+1.times do
+    puts "I'm a code block!"
+end
+  
+1.times { puts "As am I!" }
+
+# Sort
+books = ["Charlie and the Chocolate Factory", "War and Peace", "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
+books.sort!
+result = ["A Brief History of Time", "A Wrinkle in Time", "Charlie and the Chocolate Factory", "Utopia", "War and Peace"] #Sorts words alphabetically
