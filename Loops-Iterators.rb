e120130992a4bf9872972e0725c08a0c0c43b16d
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-12 16:32:03 +1000

    Update Loops-Iterators.rb

--- Loops-Iterators.rb
+++ Loops-Iterators.rb
@@ -1,3 +1,15 @@
+#Each
+hash = { bacon: 300, coconut: 200 }
+hash.each { |key,value| puts "#{key} price is #{value}" }
+
+#Each with do instead
+numbers.each do |number|
+  if number % 2 == 0
+    evens.push(number)
+  end
+end
+
+
 # While
 for i in 1..50
     print i
