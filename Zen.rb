1758522d4849966bfe122af780917cbe4518856c
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-13 14:42:56 +1000

    Create Zen.rb

---
+++ Zen.rb
@@ -0,0 +1,18 @@
+# ternary (Instead of if/else)
+puts 3 < 4 ? "3 is less than 4!" : "3 is not less than 4."
+# eval     ?      Do if true     :       do if false
+
+# Case
+puts "Hello there!"
+greeting = gets.chomp
+
+# Add your case statement below!
+case greeting
+  when "English" then puts "Hello!"
+  when "French" then puts "Bonjour!"
+  when "German" then puts "Guten Tag!"
+  when "Finnish" then puts "Haloo!"
+  else puts "I don’t know that language!"
+end
+
+#Conditional Assignment
